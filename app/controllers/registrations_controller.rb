6c026418f525653796bb74c211ef63d56ad38f0a
Icefox <icefox.990@gmail.com>
2015-11-13 10:11:17 -0300

    Pequeños detalles

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    'users#edit' # Or :prefix_to_your_route
+    current_user # Or :prefix_to_your_route
   end
 end
